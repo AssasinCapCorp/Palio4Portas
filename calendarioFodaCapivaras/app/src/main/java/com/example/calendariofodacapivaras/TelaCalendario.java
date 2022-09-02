f8a908e3c7c352f7515e574f2ae34ca1c35493ce
AssasinCapCorp <capivara.assassina.corp@gmail.com>
2022-09-02 09:38:40 -0300

    adicionar a tela calendario

---
+++ calendarioFodaCapivaras/app/src/main/java/com/example/calendariofodacapivaras/TelaCalendario.java
@@ -0,0 +1,14 @@
+package com.example.calendariofodacapivaras;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaCalendario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_calendario);
+    }
+}
